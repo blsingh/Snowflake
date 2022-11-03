c0fd1bfe1e948dd72eb02c394b9ab7caa8a8be93
blsingh <blsingh@ucdavis.edu>
2022-11-03 16:32:38 -0700

    SSCC_1; 1st pust to snfk repo

---
+++ SSCC_1.sql
@@ -0,0 +1,65 @@
+ls
+
+pwd
+
+snowsql -a ysb81193 -u cortomaltese
+-- enter password at prompt
+
+SELECT CURRENT_TIMESTAMP;
+
+SELECT CURRENT_DATE;
+
+SELECT CURRENT_DATABASE();
+
+CREATE DATABASE demo_data_loading;
+
+USE DATABASE demo_date_loading;
+
+CREATE table customer(
+    name STRING,
+    phone STRING,
+    email STRING,
+    address STRING,
+    postalCode STRING,
+    region STRING,
+    country STRING
+);
+
+
+
+SELECT * FROM customer;
+-- pass comment;
+
+SHOW TABLES;
+
+
+-- SSCC EX(ch4)
+
+CREATE OR REPLACE DATABASE demo_data_loading;
+
+USE DATABASE DEMO_DATE_LOADING;
+
+CREATE table customer
+(
+name STRING,
+phone STRING,
+email STRING,
+address STRING,
+postalCode STRING,
+region STRING,
+country STRING
+);
+
+SHOW TABLES;
+
+LIST @%customer;
+
+PUT 'file:///mnt/c/users/blsin/prompt_Git_Snowflake saves/customers.csv' @%customer;
+
+LIST @%customer;
+
+COPY INTO customer FROM @%customer FILE_FORMAT=(TYPE = 'CSV' FIELD_DELIMITER = '|' SKIP_HEADER = 1 COMPRESSION = 'GZIP');
+
+ALTER TABLE customer add led smallint default 1;
+
+SELECT * FROM customer;
